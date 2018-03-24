1440132b83753f39efe1dd14e7a6c88d698217f3
YouminYoumin <dbals2007@naver.com>
2018-03-25 03:15:40 +0900

    update 0325
    토스트 메시지 시험용으로 추가

--- sangwoo/PingPongKimMain/mobile/src/main/java/com/github/pocmo/pingpongkim/SensorReceiverService.java
+++ sangwoo/PingPongKimMain/mobile/src/main/java/com/github/pocmo/pingpongkim/SensorReceiverService.java
@@ -3,6 +3,7 @@ package com.github.pocmo.pingpongkim;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.github.pocmo.pingpongkim.shared.DataMapKeys;
 import com.google.android.gms.wearable.DataEvent;
@@ -114,6 +115,7 @@ public class SensorReceiverService extends WearableListenerService {
                 if(isHorizontalSwing)msg = "SWING";
                 else if(isBackSwing) msg = "BACK SWING";
                 else msg = "NONE";
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
             }
             else{
                 msg = "-";
